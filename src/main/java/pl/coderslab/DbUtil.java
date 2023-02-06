bc32ccd9b5d01b4becd7f13b523c7915bdcd7bd3
patryk93 <patryk.r.skubisz@gmail.com>
2023-02-06 19:24:07 +0100

    UserDao

--- src/main/java/pl/coderslab/DbUtil.java
+++ src/main/java/pl/coderslab/DbUtil.java
@@ -1,2 +1,52 @@
+package pl.coderslab;
+
+import java.sql.*;
+
 public class DbUtil {
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/workshop2?useSSL=false&characterEncoding=utf8&serverTimezone=UTC",
+                "user",
+                "pass");
+    }
+
+    public static void insert(Connection conn, String query, String... params) {
+        try (PreparedStatement statement = conn.prepareStatement(query)) {
+            for (int i = 0; i < params.length; i++) {
+                statement.setString(i + 1, params[i]);
+            }
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printData(Connection conn, String query, String... columnNames) throws SQLException {
+
+        try (PreparedStatement statement = conn.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
+                for (String columnName : columnNames) {
+                    System.out.println(resultSet.getString(columnName));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static final String DELETE_QUERY = "DELETE FROM tableName where id = ?";
+
+    public static void remove(Connection conn, String tableName, int id) {
+        try (PreparedStatement statement =
+                     conn.prepareStatement(DELETE_QUERY.replace("tableName", tableName))) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,2 +1,87 @@
+package pl.coderslab;
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserDao;
+
+import java.util.Scanner;
+
 public class Main {
+
+    static final String[] OPTIONS = {"create", "read", "update", "delete", "list", "exit"};
+    public static void main(String[] args){
+
+        User user = new User();
+        UserDao userDao = new UserDao();
+        Scanner scanner = new Scanner(System.in);
+        showOptions(OPTIONS);
+        String input = scanner.nextLine();
+        while (!"exit".equals(input)) {
+            switch (input) {
+                case "create" -> {
+                    System.out.println("Please add a UserName");
+                    user.setUserName(scanner.nextLine());
+                    System.out.println("Please add an email");
+                    user.setEmail(scanner.nextLine());
+                    System.out.println("Please add a password");
+                    user.setPassword(scanner.nextLine());
+                    userDao.create(user);
+                    System.out.println("User has been added correctly");
+                }
+                case "read" -> {
+                    System.out.println("Please add an ID");
+                    try {
+                        int id = Integer.parseInt(scanner.nextLine());
+                        user = userDao.read(id);
+                        System.out.println("User ID = " + user.getId());
+                        System.out.println("UserName = " + user.getUserName());
+                        System.out.println("Email = " + user.getEmail());
+
+
+                    } catch (NumberFormatException e) {
+                        System.out.println("Please add correct ID");
+                    }
+                }
+                case "update" -> {
+                    System.out.println("Please add an ID");
+                    try {
+                        int id = Integer.parseInt(scanner.nextLine());
+                        user = userDao.read(id);
+                        userDao.update(user);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Please type correct ID");
+                    }
+
+                }
+                case "delete" -> {
+                    System.out.println("Please add an ID");
+                    try {
+                        int id = Integer.parseInt(scanner.nextLine());
+                        userDao.delete(id);
+                        System.out.println("User has been deleted successfully");
+
+                    } catch (NumberFormatException e) {
+                        System.out.println("Please type correct ID");
+                    }
+
+                }
+                case "list" -> userDao.findAll();
+
+                default -> {
+                    System.out.println("Please select a correct option");
+                }
+
+            }
+            System.out.println();
+            showOptions(OPTIONS);
+            input = scanner.nextLine();
+        }
+        System.out.println("Bye, bye.");
+    }
+
+    public static void showOptions(String[] options) {
+        System.out.println("Please select an option: " );
+        for (String option : options) {
+            System.out.println(option);
+        }
+    }
+
 }
